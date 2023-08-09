6e44a39b988347d0bfa7be964132f3030e753f24
Adam Gutasi <40776147+gutasiadam@users.noreply.github.com>
2023-08-09 11:48:06 +0200

    23w32b
    Moving project to Docker, with updated gitignore files

---
+++ www/io/Database.php
@@ -0,0 +1,71 @@
+<?php
+namespace Mediaio;
+
+class Database{
+
+
+    static function runQuery($query){
+        // Specify the path to the JSON file
+            $file_path = $_SERVER["DOCUMENT_ROOT"].'/server/dbCredentials.json';
+			//echo $file_path;
+            // Read the contents of the file
+            $json_data = file_get_contents($file_path);
+
+            // Decode the JSON data into an associative array
+            $credentials = json_decode($json_data, true);
+
+            // Extract the username, password, and schema fields
+            $username = $credentials['username'];
+            $password = $credentials['password'];
+            $schema = $credentials['schema'];
+
+        /* Runs an SQL query on the databse, and returns it's result. 
+            - Doesn't check the query, it blindly runs it !
+            - Doesn't close the databse connection either.
+        */
+        $connection = mysqli_connect('mysql', $username, $password, $schema);
+		mysqli_set_charset($connection,"utf8mb4");
+        if (!$connection){
+            die("Connection failed: ".mysqli_connect_error());
+        }
+        //$connection->close();
+        return $connection->query($query);
+        /*$statement = mysqli_stmt_init($connection);
+        if (!mysqli_stmt_prepare($statement, $query)){
+            return $query;
+            exit();
+        }else{
+        mysqli_stmt_execute($statement);
+        $result = mysqli_stmt_get_result($statement);
+        
+        return $result;
+        }*/
+    }
+
+    //Runs query, and returns the mysqli object as a result.
+
+    //Caller object SHOULD close the connection!
+    static function runQuery_mysqli(){
+		
+            $file_path = $_SERVER["DOCUMENT_ROOT"].'/server/dbCredentials.json';
+
+            // Read the contents of the file
+            $json_data = file_get_contents($file_path);
+
+            // Decode the JSON data into an associative array
+            $credentials = json_decode($json_data, true);
+
+            // Extract the username, password, and schema fields
+            $username = $credentials['username'];
+            $password = $credentials['password'];
+            $schema = $credentials['schema'];
+            
+        $connection = mysqli_connect('mysql', $username, $password, $schema);
+		mysqli_set_charset($connection,"utf8mb4");
+        if (!$connection){
+            die("Connection failed: ".mysqli_connect_error());
+        }
+        return $connection;
+    }
+}
+?>
---
+++ www/io/utility/takeout_administrator.php
@@ -0,0 +1,66 @@
+<?php
+namespace Mediaio;
+session_start();
+require_once __DIR__.'/../Database.php';
+require_once __DIR__.'/../Core.php';
+use Mediaio\Core;
+use Mediaio\Database;
+error_reporting(E_ERROR | E_WARNING | E_PARSE );
+$SESSuserName = $_SESSION['UserUserName'];
+
+if( isset($_POST['takeoutData'])){
+  $logDump=fopen("log.txt", "w");
+  $takeoutData= ($_POST['takeoutData']['items']);
+    //var_dump($takeoutData);
+    
+    foreach ($takeoutData as $entry){
+      //echo($entry['name']);
+      fwrite($logDump, $entry['name']."\n");
+      //echo($entry['id']);
+      fwrite($logDump, $entry['id']."\n");
+    }
+    $currDate= date("Y/m/d H:i:s");
+    $mysqli = Database::runQuery_mysqli();
+    if(in_array("admin",$_SESSION['groups'])){//Auto accept 
+      //echo "Auto accept available";
+      $sql = ("INSERT INTO takelog (`ID`, `Date`, `User`, `Items`, `Event`,`Acknowledged`,`ACKBY`) VALUES (NULL, '$currDate', '$SESSuserName', '".json_encode($takeoutData)."', 'OUT',1,'$SESSuserName')");
+    }else{
+      $sql = ("INSERT INTO takelog (`ID`, `Date`, `User`, `Items`, `Event`,`Acknowledged`,`ACKBY`) VALUES (NULL, '$currDate', '$SESSuserName', '".json_encode($takeoutData)."', 'OUT',0,NULL)");
+    }
+    //echo "sql: ".$sql."\n";
+     $result=mysqli_query($mysqli,$sql);
+      if($result == TRUE){
+        //Change every item as taken in the database
+        //echo "Change every item as taken in the database";
+        foreach ($takeoutData as $i){
+          $name= $i["name"];
+          if(in_array("admin",$_SESSION['groups'])){//Auto accept complete, automatically update databse
+            //0 : unavailable
+            $sql = ("UPDATE leltar SET Status = 0, RentBy = '$SESSuserName' WHERE `Nev`='$name'");
+             //echo "Auto accept OK, sql: ".$sql."\n";
+          }else{ //Manual accept, update database with status 2
+            $sql = ("UPDATE leltar SET Status = 2, RentBy = '$SESSuserName' WHERE `Nev`='$name'");
+          }
+          $result=mysqli_query($mysqli,$sql);
+          if($result != TRUE){
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+        }
+        echo 200;
+        $mysqli->close();
+        exit();
+      }else{
+        echo 400;
+        $mysqli->close();
+        exit();
+      }
+    
+  // }
+  //Reloads items
+  include('./refetchdata.php');
+  exit;
+}else{
+  echo 500;
+  exit();
+}
+?>
