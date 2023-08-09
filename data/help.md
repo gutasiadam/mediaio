36f05070d1d909e2d59a233c6fbfa91014562592
Adam Gutasi <40776147+gutasiadam@users.noreply.github.com>
2023-08-09 11:55:21 +0200

    23w32b
    Moving project to Docker, with updated gitignore files

--- data/help.md
+++
@@ -1,2 +0,0 @@
-## Data:
-Ide töltődnek a felhasználók által feltöltött képek.
---
+++ www/io/Mailer.php
@@ -0,0 +1,54 @@
+<?php
+namespace Mediaio;
+
+require_once __DIR__ .'/vendor/autoload.php';
+require_once __DIR__ .'/Config.php';
+
+use SendinBlue\Client\Configuration;
+use SendinBlue\Client\Api\TransactionalEmailsApi;
+use SendinBlue\Client\Model\SendSmtpEmail;
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Europe/Budapest');
+
+class MailService
+{
+
+    static function sendContactMail($name,$to,$subject,$content)
+    {
+        //echo $to;
+        // Read the JSON file into a string
+        $jsonString = file_get_contents(__DIR__.'/utility/mailCredentials.json');
+
+        // Decode the JSON string into a PHP object
+        $data = json_decode($jsonString);
+
+        // Access the field using object syntax
+        $apiKey = $data->mailApiKey;
+        $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', $apiKey);
+        $apiInstance = new TransactionalEmailsApi(null, $config);
+
+        $sendSmtpEmail = new SendSmtpEmail([
+            'sender' => ['email' => 'arpadmedia.io@gmail.com', 'name' => 'Árpád Média IO'],
+            'to' => [['email' => $to]], //, 'name' => 'Adam Gutasi'
+            'htmlContent' => $content,
+            'subject' => $subject,
+        ]);
+
+        try {
+            $result = $apiInstance->sendTransacEmail($sendSmtpEmail);
+            //print_r($result);
+            $output = json_encode(array('type'=>'message', 'text' => 'OK.'));
+        } catch (Exception $e) {
+            $output = json_encode(array('type'=>'error', 'text' => $e->getMessage()));
+        }
+        return $output;
+    }
+
+    static function sendContactMailWithAttachment(){
+
+    }
+
+    
+}
